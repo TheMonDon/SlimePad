ee136389c27cc0d409644879865db9e33973552a
TheMonDon <bforster48@gmail.com>
2019-05-04 18:15:52 -0500

    Added enabledWorlds and reload command
    Must add the enabledWorlds to current config or reset config. Reload command: /spr

--- src/main/java/me/TheMonDon/SlimePad.java
+++ src/main/java/me/TheMonDon/SlimePad.java
@@ -8,6 +8,7 @@ import org.bukkit.block.BlockFace;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
@@ -28,33 +29,46 @@ public class SlimePad extends JavaPlugin implements Listener, CommandExecutor {
         distance = this.getConfig().getDouble("distance");
 
         Bukkit.getLogger().info("---------------------------");
-        Bukkit.getLogger().info("         SlimePad v1.9     ");
+        Bukkit.getLogger().info("         SlimePad v2.0     ");
         Bukkit.getLogger().info("---------------------------");
 
         Metrics metrics = new Metrics(this);
         getServer().getPluginManager().registerEvents(this, this);
         getCommand("slimepad").setExecutor(this);
-    }
-
-    @Override
-    public void onDisable() {
-        Bukkit.getLogger().info("---------------------------");
-        Bukkit.getLogger().info("         SlimePad v1.9     ");
-        Bukkit.getLogger().info("---------------------------");
+        getCommand("slimepad-reload").setExecutor(this);
     }
 
     public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
-        sender.sendMessage(ChatColor.AQUA.toString() + ChatColor.BOLD.toString() + "SlimePad");
-        sender.sendMessage(ChatColor.AQUA.toString() + "Version" + ChatColor.WHITE + ": " + ChatColor.GRAY + "v1.9");
-        sender.sendMessage(ChatColor.AQUA.toString() + "Developer" + ChatColor.WHITE + ": " + ChatColor.GRAY + "TheMonDon");
+        if (cmd.getName().equalsIgnoreCase("slimepad")) {
+            sender.sendMessage(ChatColor.AQUA.toString() + ChatColor.BOLD.toString() + "SlimePad");
+            sender.sendMessage(ChatColor.AQUA.toString() + "Version" + ChatColor.WHITE + ": " + ChatColor.GRAY + "v1.9");
+            sender.sendMessage(ChatColor.AQUA.toString() + "Developer" + ChatColor.WHITE + ": " + ChatColor.GRAY + "TheMonDon");
+        }
+        if (cmd.getName().equalsIgnoreCase("slimepad-reload")) {
+            if (sender.hasPermission("sp.reload")) {
+                reloadConfig();
+                sender.sendMessage(ChatColor.GREEN + "Configuration Reloaded!");
+            }
+            return true;
+        }
         return true;
     }
 
     @EventHandler
     public void onPlayerMove(PlayerMoveEvent e) {
-        if (e.getTo().getBlock().getRelative(BlockFace.DOWN).getType() == mat) {
-            e.getPlayer().setVelocity(e.getPlayer().getLocation().getDirection().multiply(distance));
-            e.getPlayer().setVelocity(new Vector(e.getPlayer().getVelocity().getX(), height, e.getPlayer().getVelocity().getZ()));
+        Player p = e.getPlayer();
+        if(getConfig().getStringList("enabledWorlds").contains(p.getLocation().getWorld().getName())){
+            if (e.getTo().getBlock().getRelative(BlockFace.DOWN).getType() == mat) {
+                e.getPlayer().setVelocity(e.getPlayer().getLocation().getDirection().multiply(distance));
+                e.getPlayer().setVelocity(new Vector(e.getPlayer().getVelocity().getX(), height, e.getPlayer().getVelocity().getZ()));
+            }
         }
+    }
+
+    @Override
+    public void onDisable() {
+        Bukkit.getLogger().info("---------------------------");
+        Bukkit.getLogger().info("         SlimePad v2.0     ");
+        Bukkit.getLogger().info("---------------------------");
     }
 }
